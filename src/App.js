59eab32bebef22df35db2c79be07a07053c4d773
Malbathan <malbathan.lima@wedigi.com.br>
2023-10-17 11:30:21 -0300

    Filter by brand

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import CarList from './components/carList';
+import brandData from "./utils/dataCarsByBrand.json"
 
 function App() {
   return (
     <div className="App">
       <p> Hello world!</p>
-      <CarList />
+      <CarList brands={brandData.brands} cars={brandData.cars}/>
     </div>
   );
 }
--- src/components/carList.js
+++ src/components/carList.js
@@ -1,22 +1,57 @@
-import React, { useEffect, useState } from "react";
-import carData from "../utils/dataCars.json"
-import brandData from "../utils/dataCarsByBrand.json"
+import React, { useState } from "react";
 
-function CarList() {
-  const [cars, setCars] = useState([]);
-  const [brands, setBrands] = useState([]);
+const CarList = ({ brands , cars }) => {
+  const [selectedBrand, setSelectedBrand] = useState(null);
+	console.log("TCL: CarList ->", selectedBrand)
 
-  useEffect(() => {
-    setCars(carData.cars);
-    setBrands(brandData.cars);
-  }, []);
+  console.log(cars.filter((car) => car.brand === selectedBrand ))
+
+  const handleBrandFilter = (brand) => {
+    setSelectedBrand(brand);
+
+  };
+
+  const filterCars = (selectedBrand) => {
+    if (selectedBrand === "TODOS") {
+      return [...cars];
+    }
+
+    const brandId = parseInt(selectedBrand);
+
+    return selectedBrand
+      ? cars.filter((car) => car.brand === brandId )
+      : cars;
+  };
+
+  const filteredCars = filterCars(selectedBrand);
 
   return (
     <div>
       <h1>Lista de Carros</h1>
-      
+      <div>
+        <label>Filtrar por Marca:</label>
+        <select onChange={(e) => handleBrandFilter(e.target.value)}>
+          <option value="TODOS">Todos</option>
+          {brands.map((brand) => (
+            <option key={brand.id} value={brand.id}>
+              {brand.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <ul>
+        {filteredCars.map((car) => (
+          <li key={car.id}>
+            <h2>{car.nome_modelo}</h2>
+            <p>Ano: {car.ano}</p>
+            <p>Combustível: {car.combustivel}</p>
+            <p>Cor: {car.cor}</p>
+            <p>Valor: R$ {car.valor}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default CarList;
