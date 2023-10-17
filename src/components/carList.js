e3f228ddfce876df34a5029b5100b411607b4c13
Malbathan <malbathan.lima@wedigi.com.br>
2023-10-17 12:37:40 -0300

    Add Styled components to our project

--- src/components/carList.js
+++ src/components/carList.js
@@ -1,29 +1,54 @@
 import React, { useState } from "react";
 
-const CarList = ({ brands , cars }) => {
-  const [selectedBrand, setSelectedBrand] = useState(null);
-	console.log("TCL: CarList ->", selectedBrand)
-
-  console.log(cars.filter((car) => car.brand === selectedBrand ))
+const CarList = ({ brands, cars }) => {
+  const [selectedBrand, setSelectedBrand] = useState("TODOS");
+  const [selectedYearFilters, setSelectedYearFilters] = useState([]);
+  const [selectedFuelFilters, setSelectedFuelFilters] = useState([]);
+  const [selectedColorFilters, setSelectedColorFilters] = useState([]);
 
   const handleBrandFilter = (brand) => {
     setSelectedBrand(brand);
-
   };
 
-  const filterCars = (selectedBrand) => {
-    if (selectedBrand === "TODOS") {
-      return [...cars];
+  const handleYearFilter = (year) => {
+    if (selectedYearFilters.includes(year)) {
+      setSelectedYearFilters(selectedYearFilters.filter((filter) => filter !== year));
+    } else {
+      setSelectedYearFilters([...selectedYearFilters, year]);
     }
+  };
+
+  const handleFuelFilter = (fuel) => {
+    if (selectedFuelFilters.includes(fuel)) {
+      setSelectedFuelFilters(selectedFuelFilters.filter((filter) => filter !== fuel));
+    } else {
+      setSelectedFuelFilters([...selectedFuelFilters, fuel]);
+    }
+  };
+
+  const handleColorFilter = (color) => {
+    if (selectedColorFilters.includes(color)) {
+      setSelectedColorFilters(selectedColorFilters.filter((filter) => filter !== color));
+    } else {
+      setSelectedColorFilters([...selectedColorFilters, color]);
+    }
+  };
+
+  const filterCars = () => {
 
     const brandId = parseInt(selectedBrand);
 
-    return selectedBrand
-      ? cars.filter((car) => car.brand === brandId )
-      : cars;
+    return cars.filter((car) => {
+      return (
+        (selectedBrand === "TODOS" || car.brand === brandId) &&
+        (selectedYearFilters.length === 0 || selectedYearFilters.includes(car.ano)) &&
+        (selectedFuelFilters.length === 0 || selectedFuelFilters.includes(car.combustivel)) &&
+        (selectedColorFilters.length === 0 || selectedColorFilters.includes(car.cor))
+      );
+    });
   };
 
-  const filteredCars = filterCars(selectedBrand);
+  const filteredCars = filterCars();
 
   return (
     <div>
@@ -38,6 +63,60 @@ const CarList = ({ brands , cars }) => {
             </option>
           ))}
         </select>
+      </div>
+      <div>
+        <label>Filtrar por Ano:</label>
+        <ul>
+          {[2014, 2015, 1993].map((year) => (
+            <li key={year}>
+              <label>
+                <input
+                  type="checkbox"
+                  value={year}
+                  checked={selectedYearFilters.includes(year)}
+                  onChange={() => handleYearFilter(year)}
+                />
+                {year}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <label>Filtrar por Combustível:</label>
+        <ul>
+          {["FLEX", "DIESEL"].map((fuel) => (
+            <li key={fuel}>
+              <label>
+                <input
+                  type="checkbox"
+                  value={fuel}
+                  checked={selectedFuelFilters.includes(fuel)}
+                  onChange={() => handleFuelFilter(fuel)}
+                />
+                {fuel}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <label>Filtrar por Cor:</label>
+        <ul>
+          {["BRANCA", "PRETO", "AZUL", "BEGE"].map((color) => (
+            <li key={color}>
+              <label>
+                <input
+                  type="checkbox"
+                  value={color}
+                  checked={selectedColorFilters.includes(color)}
+                  onChange={() => handleColorFilter(color)}
+                />
+                {color}
+              </label>
+            </li>
+          ))}
+        </ul>
       </div>
       <ul>
         {filteredCars.map((car) => (
